9c86c46c5cf166285c11fec97ea6d74e3213513d
Missteen21 <mmiss.tch@gmail.com>
2017-04-20 01:57:18 +0700

    Update main.php

--- main.php
+++ main.php
@@ -1,6 +1,13 @@
-<html>
-    <body> bgcolor="#cc33cc" </body>
-
-<?php
-    echo 'Hello world!';
+<!doctype html>
+<html lang="th">
+<head>
+ <font size="5">
+ <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<body background="b.jpg">
 </html>
+<br><center><img src="king.jpg"width="400" height="400"/></center><br></font>
+<font color=#FFFFCC ; font size=20><font face="KodchiangUPC"> <center>ปวงข้าพระพุทธเจ้า  <br><center>ขอน้อมเกล้าน้อมกระหม่อมรำลึกในพระมหากรุณาธิคุณหาที่สุดมิได้</b></center></font></font>
+<br><a href="main2.php"><b style="color:##FFFF00"><h3><u>เข้าสู่หน้าหลักเว็บไซต์</u></h3></a></b><br>
