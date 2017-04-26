56641e188c64b5984da53a5e67739f9349c792d9
Missteen21 <mmiss.tch@gmail.com>
2017-04-26 21:46:25 +0700

    Add files via upload

---
+++ main3.php
@@ -0,0 +1,338 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Motorcycle Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!-- styles -->
+<link rel="stylesheet" href="assets/css/fancybox/jquery.fancybox.css">
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<link href="assets/css/bootstrap-theme.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/css/slippry.css">
+<link href="assets/css/style.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/color/default.css">
+<!-- =======================================================
+    Theme Name: Groovin
+    Theme URL: https://bootstrapmade.com/groovin-free-bootstrap-theme/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+======================================================= -->
+<script src="assets/js/modernizr.custom.js"></script>
+</head>
+<body>
+<header>
+
+<div id="navigation" class="navbar navbar-inverse navbar-fixed-top default" role="navigation">
+  <div class="container">
+<!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <h1><b>Motorcycle</h1></b><br>
+	  
+    </div>
+
+	
+      <ul class="nav navbar-nav navbar-right">
+        <li class="current"><a href="main2.php">Home</a></li>
+		<li><a href="history.php">About</a></li>
+		<li><a href="main3.php">Works</a></li>
+		<li><a href="#login">Login</a></li>
+      </ul></nav>
+
+  </div>
+</div>
+
+</header>
+<!-- end section about -->
+<!-- section works -->
+<section id="works" class="section gray">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="heading">
+				<h3><span>Recent works</span></h3>
+			</div>
+		</div>
+	</div>
+	
+</style>
+<center><h4>&nbsp;&nbsp&nbsp;&nbsp<u>จงเซิน ริวก้า / Zongshen Ryuka</h4></center></u></h2><br>
+<h5>&nbsp;&nbsp&nbsp;&nbsp RX3</h5></h2><br>
+<center><img src="c1.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : ZS250GY-3<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : RYUKA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : RX3<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  159,000.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  129,000.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp Zone</h5></h2><br>
+<center><img src="c2.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : Zone<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : RYUKA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : Zone<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  69,500.00   <br>  
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  59,500.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp CRUISER</h5></h2><br>
+<center><img src="c3.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : ZS125-30<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : RYUKA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : CRUISER<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  49,500.00 <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  42,900.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp RA1R</h5></h2><br>
+<center><img src="c4.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : RA1R<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : RYUKA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : RA1R<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  59,900.00     <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  57,900.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp BULL</h5></h2><br>
+<center><img src="c5.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : 3W BULL<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : RYUKA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : BULL<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  69,500.00     <br> 
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  59,500.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp BREAKOUT</h5></h2><br>
+<center><img src="c6.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : RK150<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : RYUKA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : BREAKOUT<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  65,900.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  59,900.00<br>
+<hr size="2">
+<br>
+<br>
+
+<center><h4>&nbsp;&nbsp&nbsp;&nbsp<u>Lifan</h4></center></u></h2><br>
+<h5>&nbsp;&nbsp&nbsp;&nbsp Lifan KPR150</h5></h2><br>
+<center><img src="d1.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : KPR150<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Lifan<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : Lifan KPR150<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  76,500.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  64,900.00<br>
+<hr size="2">
+<br>
+<br>
+<center><h4>&nbsp;&nbsp&nbsp;&nbsp<u>STALLIONS (สตาเลียน)</h4></center></u></h2><br>
+<h5>&nbsp;&nbsp&nbsp;&nbsp CENTAUR</h5></h2><br>
+<center><img src="d2.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : Stallions CENTAUR<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Stallions<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : CENTAUR<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  64,900.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  59,900.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp CARINO</h5></h2><br>
+<center><img src="d3.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : CARINO2015<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : STALLIONS<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : CARINO<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  48,500.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  42,500.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp MoMo Racer 2013</h5></h2><br>
+<center><img src="d4.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : SMM125A 2013<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : STALLIONS<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : MoMo Racer 2013<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  7,989.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  5,989.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp BAJA</h5></h2><br>
+<center><img src="d5.jpg"width="600" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : Stallions MiNi Motad รุ่น JR11<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Stallions MiNi Motad<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : BAJA<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  44,500.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  39,500.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp @1</h5></h2><br>
+<center><img src="d6.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : Stallions MiNi GP<br> 
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Stallions MiNi GP<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : @1<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  69,900.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  65,900.00<br>
+<hr size="2">
+
+<br>
+<br>
+<center><h4>&nbsp;&nbsp&nbsp;&nbsp<u>HONDA</h4></center></u></h2><br>
+<h5>&nbsp;&nbsp&nbsp;&nbsp HONDA Wave110i</h5></h2><br>
+<center><img src="e1.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : HONDA Wave110i<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : HONDA Wave<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : AFS110KDFF2TH<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  40,300.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  36,900.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp CBR150R</h5></h2><br>
+<center><img src="e2.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : CS150R<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : CBR150R<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : CBR150R<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  80,000.00 <br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp MSX125</h5></h2><br>
+<center><img src="e3.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : HONDA MSX125<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Honda<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : MSX125<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  76,500.00    <br> 
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  68,900.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp MOOVE</h5></h2><br>
+<center><img src="e4.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : MOOVE<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Honda<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : MOOVE<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  56,000.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  48,500.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp ZoomerX</h5></h2><br>
+<center><img src="e5.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : ZoomerX<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : Honda<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : ZoomerX<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  53,500.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  52,500.00<br>
+<hr size="2">
+<h5>&nbsp;&nbsp&nbsp;&nbsp FORZA300</h5></h2><br>
+<center><img src="e6.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : HONDA FORZA300-2015<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : HONDA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : FORZA300<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  169,000.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  159,000.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp CB300F</h5></h2><br>
+<center><img src="e7.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : CB300F<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : HONDA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : CB300F<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  136,000.00<br>      
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  126,000.00<br>
+<hr size="2">
+
+<h5>&nbsp;&nbsp&nbsp;&nbsp CB300F</h5></h2><br>
+<center><img src="e8.jpg"width="500" height="400"/></center><br></font><br>
+&nbsp;&nbsp&nbsp;&nbspรหัส : CRF250L-2015<br>
+&nbsp;&nbsp&nbsp;&nbspยี่ห้อ : HONDA<br>
+&nbsp;&nbsp&nbsp;&nbspรุ่น : CRF250L<br>
+&nbsp;&nbsp&nbsp;&nbspราคาปกติ :  154,000.00      <br>
+&nbsp;&nbsp&nbsp;&nbspราคาพิเศษ :  144,000.00<br>
+<hr size="2">
+
+<br>
+<br>
+<center><h4>&nbsp;&nbsp&nbsp;&nbsp<u>YAMAHA</h4></center></u></h2><br>
+<h5>&nbsp;&nbsp&nbsp;&nbsp HONDA Wave110i</h5></h2><br>
+<center><img src=".jpg"width="500" height="400"/></center><br></font><br>
+
+<!-- end section contact -->
+<footer>
+<div class="verybottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="aligncenter">
+                <ul class="social-network social-circle">
+                    <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                </ul>	
+				</div>				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="aligncenter">
+					<p>
+						 Department of Computer Science
+					</p>
+                    <div class="credits">
+                        <!-- 
+                            All the links in the footer should remain intact. 
+                            You can delete the links only if you purchased the pro version.
+                            Licensing information: https://bootstrapmade.com/license/
+                            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Groovin
+                        -->
+                      
+					  <a href='http://www.sc.kku.ac.th'>Faculty of Science</a> ,  <a href='http://www.kku.ac.th'>KhonKaen University</a>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
+<a href="#" class="scrollup"><i class="fa fa-angle-up fa-2x"></i></a>
+<!-- javascript -->
+<script src="assets/js/jquery-1.9.1.min.js"></script>
+<script src="assets/js/jquery.easing.js"></script>
+<script src="assets/js/classie.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/slippry.min.js"></script>
+<script src="assets/js/nagging-menu.js"></script>
+<script src="assets/js/jquery.nav.js"></script>
+<script src="assets/js/jquery.scrollTo.js"></script>
+<script src="assets/js/jquery.fancybox.pack.js"></script> 
+<script src="assets/js/jquery.fancybox-media.js"></script> 
+<script src="assets/js/masonry.pkgd.min.js"></script>
+<script src="assets/js/imagesloaded.js"></script>
+<script src="assets/js/jquery.nicescroll.min.js"></script>
+<script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="assets/js/AnimOnScroll.js"></script>
+    <script>
+        new AnimOnScroll( document.getElementById( 'grid' ), {
+            minDuration : 0.4,
+            maxDuration : 0.7,
+            viewportFactor : 0.2
+        } );
+    </script>
+<script>
+	$(document).ready(function(){
+	  $('#slippry-slider').slippry(
+		defaults = {
+			transition: 'vertical',
+			useCSS: true,
+			speed: 5000,
+			pause: 3000,
+			initSingle: false,
+			auto: true,
+			preload: 'visible',
+			pager: false,		
+		} 
+	  
+	  )
+	});
+</script>
+
+<script src="assets/js/custom.js"></script>
+<script src="contactform/contactform.js"></script>
+
+</body>
+</html>
+	
+	
+	
