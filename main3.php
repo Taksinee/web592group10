854a5c9f5eecf4aed7308aded132a2cb6dbb925f
kanjanaporn11 <k_boonrueng@kkumail.com>
2017-05-02 20:40:05 +0700

    Add files via upload

---
+++ main3.php
@@ -0,0 +1,180 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Motorcycle Page</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="description" content="" />
+<meta name="author" content="" />
+<!-- styles -->
+<link rel="stylesheet" href="assets/css/fancybox/jquery.fancybox.css">
+<link href="assets/css/bootstrap.css" rel="stylesheet" />
+<link href="assets/css/bootstrap-theme.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/css/slippry.css">
+<link href="assets/css/style.css" rel="stylesheet" />
+<link rel="stylesheet" href="assets/color/default.css">
+<!-- =======================================================
+    Theme Name: Groovin
+    Theme URL: https://bootstrapmade.com/groovin-free-bootstrap-theme/
+    Author: BootstrapMade
+    Author URL: https://bootstrapmade.com
+======================================================= -->
+<script src="assets/js/modernizr.custom.js"></script>
+</head>
+<body>
+<header>
+
+<div id="navigation" class="navbar navbar-inverse navbar-fixed-top default" role="navigation">
+  <div class="container">
+<!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <h1><b>Motorcycle</h1></b><br>
+	  
+    </div>
+
+	<ul class="nav navbar-nav navbar-right">
+        <li class="current"><a href="main2.php">Home</a></li>
+		<li><a href="history.php">About</a></li>
+		<li><a href="main3.php">Works</a></li>
+		<li><a href="contact.php">Contact</a></li>
+		<li><a href="login.php">Login</a></li>
+      </ul></nav>
+  </div>
+</div>
+
+</header>
+<!-- end section about -->
+<!-- section works -->
+<section id="works" class="section gray">
+<div class="container">
+	<div class="row">
+		<div class="col-md-8 col-md-offset-2">
+			<div class="heading">
+				<h3><span>Recent works</span></h3>
+			</div>
+		</div>
+	</div>
+	
+<div class="row">
+		<div class="col-md-12">
+						<ul class="grid effect" id="grid">
+						
+						<li><a href="assets/img/portfolio/1.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : RYUKA  รุ่น : RX3   รหัส : ZS250GY-3"><img src="assets/img/portfolio/1.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/2.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : RYUKA  รุ่น :Zone รหัส : Zone"><img src="assets/img/portfolio/2.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/3.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : RYUKA  รุ่น :CRUISER  รหัส : ZS125-30"><img src="assets/img/portfolio/3.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/4.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : RYUKA  รุ่น :RA1R  รหัส : RA1R"><img src="assets/img/portfolio/4.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/5.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : RYUKA  รุ่น :BULL  รหัส : 3W BULL"><img src="assets/img/portfolio/5.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/6.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : RYUKA  รุ่น :BREAKOUT  รหัส : RK150"><img src="assets/img/portfolio/6.jpg" alt="" /></a></li>
+						
+						<li><a href="assets/img/portfolio/7.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Lifan  รุ่น :Lifan KPR150  รหัส : KPR150"><img src="assets/img/portfolio/7.jpg" alt="" /></a></li>
+						
+						<li><a href="assets/img/portfolio/8.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : Stallions  รุ่น :CENTAUR  รหัส : Stallions CENTAUR"><img src="assets/img/portfolio/8.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/9.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : Stallions  รุ่น :CARINO  รหัส : CARINO2015"><img src="assets/img/portfolio/9.jpg" alt="" /></a></li>  
+						<li><a href="assets/img/portfolio/10.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : Stallions  รุ่น :MoMo Racer 2013  รหัส : SMM125A 2013"><img src="assets/img/portfolio/10.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/11.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : Stallions  รุ่น :BAJA  รหัส : Stallions MiNi Motad รุ่น JR11	"><img src="assets/img/portfolio/11.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/12.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : Stallions  รุ่น : @1   รหัส : Stallions MiNi GP	"><img src="assets/img/portfolio/12.jpg" alt="" /></a></li>
+						
+						<li><a href="assets/img/portfolio/13.jpg" class="fancybox" data-fancybox-group="gallery" title=" ยี่ห้อ : HONDA Wave  รุ่น : AFS110KDFF2TH  รหัส : HONDA Wave110i"><img src="assets/img/portfolio/13.jpg" alt="" /></a></li> 
+						<li><a href="assets/img/portfolio/14.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : CBR150R  รุ่น : CBR150R  รหัส : CS150R"><img src="assets/img/portfolio/14.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/15.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Honda  รุ่น : MSX125  รหัส : HONDA MSX125"><img src="assets/img/portfolio/15.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/16.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Honda  รุ่น : MOOVE  รหัส : MOOVE"><img src="assets/img/portfolio/16.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/17.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Honda  รุ่น : ZoomerX  รหัส : ZoomerX"><img src="assets/img/portfolio/17.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/18.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Honda  รุ่น : FORZA300  รหัส : HONDA FORZA300-2015"><img src="assets/img/portfolio/18.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/19.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Honda  รุ่น : CB300F  รหัส : CB300F"><img src="assets/img/portfolio/19.jpg" alt="" /></a></li>
+						<li><a href="assets/img/portfolio/20.jpg" class="fancybox" data-fancybox-group="gallery" title="ยี่ห้อ : Honda  รุ่น : CRF250L  รหัส : CRF250L-2015"><img src="assets/img/portfolio/20.jpg" alt="" /></a></li>
+						</ul>
+		</div>
+	</div>
+<!-- end section contact -->
+<footer>
+<div class="verybottom">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="aligncenter">
+                <ul class="social-network social-circle">
+                    <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                    <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                    <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                </ul>	
+				</div>				
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="aligncenter">
+					<p>
+						 Department of Computer Science
+					</p>
+                    <div class="credits">
+                        <!-- 
+                            All the links in the footer should remain intact. 
+                            You can delete the links only if you purchased the pro version.
+                            Licensing information: https://bootstrapmade.com/license/
+                            Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=Groovin
+                        -->
+                      
+					  <a href='http://www.sc.kku.ac.th'>Faculty of Science</a> ,  <a href='http://www.kku.ac.th'>KhonKaen University</a>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</footer>
+<a href="#" class="scrollup"><i class="fa fa-angle-up fa-2x"></i></a>
+<!-- javascript -->
+<script src="assets/js/jquery-1.9.1.min.js"></script>
+<script src="assets/js/jquery.easing.js"></script>
+<script src="assets/js/classie.js"></script>
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/slippry.min.js"></script>
+<script src="assets/js/nagging-menu.js"></script>
+<script src="assets/js/jquery.nav.js"></script>
+<script src="assets/js/jquery.scrollTo.js"></script>
+<script src="assets/js/jquery.fancybox.pack.js"></script> 
+<script src="assets/js/jquery.fancybox-media.js"></script> 
+<script src="assets/js/masonry.pkgd.min.js"></script>
+<script src="assets/js/imagesloaded.js"></script>
+<script src="assets/js/jquery.nicescroll.min.js"></script>
+<script src="https://maps.google.com/maps/api/js?sensor=true"></script>
+<script src="assets/js/AnimOnScroll.js"></script>
+    <script>
+        new AnimOnScroll( document.getElementById( 'grid' ), {
+            minDuration : 0.4,
+            maxDuration : 0.7,
+            viewportFactor : 0.2
+        } );
+    </script>
+<script>
+	$(document).ready(function(){
+	  $('#slippry-slider').slippry(
+		defaults = {
+			transition: 'vertical',
+			useCSS: true,
+			speed: 5000,
+			pause: 3000,
+			initSingle: false,
+			auto: true,
+			preload: 'visible',
+			pager: false,		
+		} 
+	  
+	  )
+	});
+</script>
+
+<script src="assets/js/custom.js"></script>
+<script src="contactform/contactform.js"></script>
+
+</body>
+</html>
+	
+	
+	
